9fb9194cc2f246c81a5c190a74c6f30afd5e7af8
Martin <martincorona007@hotmail.com>
2021-09-06 12:24:58 -0500

    adding UI back and front

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CalcComponent } from './calc/calc.component';
 @NgModule({
@@ -9,7 +9,8 @@ import { CalcComponent } from './calc/calc.component';
     CalcComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-app/src/app/calc/calc.component.ts
+++ angular-app/src/app/calc/calc.component.ts
@@ -5,5 +5,10 @@ import { Component } from "@angular/core";
     styleUrls: ['calc.component.css']
 })
 export class CalcComponent{
-
+    public number1: number = 0;
+    public number2: number = 0;
+    public result: number = 0;
+    public add(){
+        this.result = this.number1 + this.number2;
+    }
 }
