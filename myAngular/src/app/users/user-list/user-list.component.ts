52024b217d6929793fd2ebc927d2d43e077cf577
Martin <martincorona007@hotmail.com>
2021-09-07 14:32:03 -0500

    scoth adding lazy loading

---
+++ myAngular/src/app/users/user-list/user-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  template: `
+    <p>
+      user-list works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class UserListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ myAngular/src/app/users/user-single/user-single.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-single',
+  template: `
+    <p>
+      user-single works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class UserSingleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
