abb8ef692a9f7cad1973fc053f942d56c606b0d1
Martin <martincorona007@hotmail.com>
2021-09-07 13:55:10 -0500

    scoth form validations

--- myAngular/src/app/components/contact/contact.component.ts
+++ myAngular/src/app/components/contact/contact.component.ts
@@ -14,13 +14,43 @@ import { Component, OnInit } from '@angular/core';
       <div class="container">
         <form (ngSubmit)="processForm()">
           <div class="field">
-            <input type="text" name="name" class="input" placeholder="Your name" [(ngModel)]="name">
+            <input 
+            type="text" 
+            name="name" 
+            class="input" 
+            placeholder="Your name" 
+            [(ngModel)]="name" 
+            required 
+            minglength="3"
+            #nameInput="ngModel">
+            <div class="help is-error" *ngIf="nameInput.invalid && nameInput.dirty">
+              Name is required and needs to be at least 3 characters long.
+            </div>
           </div>
           <div class="field">
-            <input type="email" name="email" class="input" placeholder="Your email" [(ngModel)]="email">
+            <input 
+            type="email" 
+            name="email" 
+            class="input" 
+            placeholder="Your email" 
+            [(ngModel)]="email" 
+            required email
+            #emailInput="ngModel">
+            <div class="help is-error" *ngIf="emailInput.invalid && emailInput.dirty">
+              Needs to be a valid email
+            </div>
           </div>
           <div class="field">
-            <textarea class="textarea" name="message" placeholder="Whats on your mind?" [(ngModel)]="message"></textarea>
+            <textarea 
+            class="textarea" 
+            name="message" 
+            placeholder="Whats on your mind?" 
+            [(ngModel)]="message" 
+            required
+            #messageInput="ngModel"></textarea>
+            <div class="help is-error" *ngIf="emailInput.invalid && emailInput.dirty">
+              Your message is required!
+            </div>
           </div>
           <button type="submit" class="button is-danger is-large">Submit</button>
         </form>
