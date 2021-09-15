1f993a5bc24eccd2c0f308a57152c4b13e32dce3
Martin <martincorona007@hotmail.com>
2021-09-14 19:15:41 -0500

    Traversy media adding routing and button ui service

--- angular-crash/src/app/components/add-task/add-task.component.ts
+++ angular-crash/src/app/components/add-task/add-task.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit ,Output, EventEmitter} from '@angular/core';
-
+import { UiService } from 'src/app/services/ui.service';
 import {Task} from '../../Task';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
@@ -11,7 +12,12 @@ export class AddTaskComponent implements OnInit {
   text: string = '';
   day: string = '';
   reminder: boolean = false;
-  constructor() { }
+  showAddTask: boolean | undefined;
+  subscription: Subscription | undefined;
+  constructor(private uiService: UiService) {
+    
+    this.subscription = this.uiService.onToggle().subscribe((value) =>  (this.showAddTask = value));
+   }
 
   ngOnInit(): void {
   }
